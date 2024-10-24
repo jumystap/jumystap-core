9483e0206c74d5d652ce1c304a2c2e9235411607
aidosgal <agalimzhan928@gmail.com>
2024-10-24 14:44:08 +0500

    fix: make nullable work_time and work_hours

--- internal/model/announcement.go
+++ internal/model/announcement.go
@@ -1,16 +1,18 @@
 package model
 
+import "database/sql"
+
 type Announcement struct {
-    Id          string      `json:"id"`
-    Title       string      `json:"title"`
-    Description string      `json:"description"`
-    Cost        interface{} `json:"cost"`
-    CostMin     interface{} `json:"cost_min"`
-    CostMax     interface{} `json:"cost_max"`
-    SalaryType  string      `json:"salary_type"`
-    Experience  string      `json:"experience"`
-    Education   string      `json:"education"`
-    WorkTime    string      `json:"work_time"`
-    WorkHours   string      `json:"work_hours"`
-    City        string      `json:"city"`
+    Id          string          `json:"id"`
+    Title       string          `json:"title"`
+    Description string          `json:"description"`
+    Cost        interface{}     `json:"cost"`
+    CostMin     interface{}     `json:"cost_min"`
+    CostMax     interface{}     `json:"cost_max"`
+    SalaryType  string          `json:"salary_type"`
+    Experience  string          `json:"experience"`
+    Education   string          `json:"education"`
+    WorkTime    sql.NullString  `json:"work_time"`
+    WorkHours   sql.NullString  `json:"work_hours"`
+    City        string          `json:"city"`
 }
